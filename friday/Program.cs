27baffd6899f05b274a8e3c3af600b7c01e128d9
Opilane TTHK <opilane@tthk.ee>
2020-11-13 09:07:51 +0200

    first

---
+++ friday/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace friday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\opilane\samples";
+            string directoriesTxt = @"C:\Users\opilane\Friday\sampleDirectories.txt";
+            string filesTxt = @"C:\Users\opilane\Friday\sampleFiles.txt";
+
+            string[] includeNestedDirecotries = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            string[] allFiles = Directory.GetFiles(rootPath, "*", SearchOption.AllDirectories);
+
+            File.WriteAllLines(directoriesTxt, includeNestedDirecotries);
+            File.WriteAllLines(filesTxt, allFiles);
+            Console.WriteLine("Complete");
+        }
+    }
+}
